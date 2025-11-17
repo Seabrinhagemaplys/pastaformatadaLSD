dff4ac3ae8a412c8e4196175cf89d1606cb98e9a
Seabrinhagemaplys <66424663+Seabrinhagemaplys@users.noreply.github.com>
2025-11-17 15:35:17 -0300

    Update testbench -- Arthur Coronho, Lucas Szuster, Murillo Kelvin - Turma PN5.vhd

--- blocos_logicos - Arthur Coronho, Lucas Szuster Murillo Kelvin/calcular_virar_a_direita - Arthur Coronho, Lucas Szuster Murillo Kelvin - Turma PN5/testbench -- Arthur Coronho, Lucas Szuster, Murillo Kelvin - Turma PN5.vhd
+++ blocos_logicos - Arthur Coronho, Lucas Szuster Murillo Kelvin/calcular_virar_a_direita - Arthur Coronho, Lucas Szuster Murillo Kelvin - Turma PN5/testbench -- Arthur Coronho, Lucas Szuster, Murillo Kelvin - Turma PN5.vhd
@@ -13,6 +13,7 @@ architecture sim of tb_calcular_virar_a_direita is
     signal emergencia : std_logic;
     signal outra_emergencia : std_logic;
     constant tempo : time := 10 ns;
+    signal clear_assincrono : std_logic := '0';
 begin
 	-- inicializando a uut e realizando o port map
 	uut: entity work.calcular_virar_a_direita
@@ -22,12 +23,128 @@ begin
         outro_pedestre => outro_pedestre,
         emergencia => emergencia,
         outra_emergencia => outra_emergencia,
-        virar_a_direita => virar_a_direita
+        virar_a_direita => virar_a_direita,
+        clear_assincrono => clear_assincrono
     );
     
     stim: process
     begin
     	-- teste 1: 0 0 0 0
+		pedestre <= '0'; 
+        outro_pedestre <= '0'; 
+        emergencia <= '0'; 
+        outra_emergencia <= '0'; 
+        wait for tempo;
+        
+        -- teste 2: 0 0 0 1
+		pedestre <= '0'; 
+        outro_pedestre <= '0'; 
+        emergencia <= '0'; 
+        outra_emergencia <= '1'; 
+        wait for tempo;
+        
+        -- teste 3: 0 0 1 0
+		pedestre <= '0'; 
+        outro_pedestre <= '0'; 
+        emergencia <= '1'; 
+        outra_emergencia <= '0'; 
+        wait for tempo;
+        
+        -- teste 4: 0 0 1 1
+		pedestre <= '0'; 
+        outro_pedestre <= '0'; 
+        emergencia <= '1'; 
+        outra_emergencia <= '1'; 
+        wait for tempo;
+        
+        -- teste 5: 0 1 0 0
+		pedestre <= '0'; 
+        outro_pedestre <= '1'; 
+        emergencia <= '0'; 
+        outra_emergencia <= '0'; 
+        wait for tempo;
+        
+        -- teste 6: 0 1 0 1
+		pedestre <= '0'; 
+        outro_pedestre <= '1'; 
+        emergencia <= '0'; 
+        outra_emergencia <= '1'; 
+        wait for tempo;
+        
+        -- teste 7: 0 1 1 0
+		pedestre <= '0'; 
+        outro_pedestre <= '1'; 
+        emergencia <= '1'; 
+        outra_emergencia <= '0'; 
+        wait for tempo;
+        
+        -- teste 8: 0 1 1 1
+		pedestre <= '0'; 
+        outro_pedestre <= '1'; 
+        emergencia <= '1'; 
+        outra_emergencia <= '1'; 
+        wait for tempo;
+        
+        -- teste 9: 1 0 0 0
+		pedestre <= '1'; 
+        outro_pedestre <= '0'; 
+        emergencia <= '0'; 
+        outra_emergencia <= '0'; 
+        wait for tempo;
+        
+        -- teste 10: 1 0 0 1
+		pedestre <= '1'; 
+        outro_pedestre <= '0'; 
+        emergencia <= '0'; 
+        outra_emergencia <= '1'; 
+        wait for tempo;
+        
+        -- teste 11: 1 0 1 0
+		pedestre <= '1'; 
+        outro_pedestre <= '0'; 
+        emergencia <= '1'; 
+        outra_emergencia <= '0'; 
+        wait for tempo;
+        
+        -- teste 12: 1 0 1 1
+		pedestre <= '1'; 
+        outro_pedestre <= '0'; 
+        emergencia <= '1'; 
+        outra_emergencia <= '1'; 
+        wait for tempo;
+        
+        -- teste 13: 1 1 0 0
+		pedestre <= '1'; 
+        outro_pedestre <= '1'; 
+        emergencia <= '0'; 
+        outra_emergencia <= '0'; 
+        wait for tempo;
+        
+        -- teste 14: 1 1 0 1
+		pedestre <= '1'; 
+        outro_pedestre <= '1'; 
+        emergencia <= '0'; 
+        outra_emergencia <= '1'; 
+        wait for tempo;
+        
+        -- teste 15: 1 1 1 0
+		pedestre <= '1'; 
+        outro_pedestre <= '1'; 
+        emergencia <= '1'; 
+        outra_emergencia <= '0'; 
+        wait for tempo;
+        
+        -- teste 16: 1 1 1 1
+		pedestre <= '1'; 
+        outro_pedestre <= '1'; 
+        emergencia <= '1'; 
+        outra_emergencia <= '1'; 
+        wait for tempo;
+        
+        -- Testes agora com clear_assincrono = 1:
+        
+        clear_assincrono <='1';
+        -- teste 1: 0 0 0 0
 		pedestre <= '0'; 
         outro_pedestre <= '0'; 
         emergencia <= '0'; 
