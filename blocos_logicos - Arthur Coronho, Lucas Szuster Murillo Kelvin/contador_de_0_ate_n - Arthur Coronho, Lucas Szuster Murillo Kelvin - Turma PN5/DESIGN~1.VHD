ae85a3b33a2d5472b3343a40179f553d449ddb08
Seabrinhagemaplys <66424663+Seabrinhagemaplys@users.noreply.github.com>
2025-11-16 22:56:34 -0300

    Add files via upload

---
+++ blocos_logicos - Arthur Coronho, Lucas Szuster Murillo Kelvin/contador_de_0_ate_n - Arthur Coronho, Lucas Szuster Murillo Kelvin - Turma PN5/DESIGN~1.VHD
@@ -0,0 +1,47 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity contador_n is
+  generic (
+    -- quantidade de contagem
+    N : integer := 4
+  );
+  port (
+    -- entradas
+    clear_contador : in std_logic;
+    clk : in std_logic;
+    
+    -- saídas
+    count : out std_logic := '0'
+  );
+end entity;
+
+architecture cnt of contador_n is
+	-- declaração de um signal para realizar as contas
+	signal contador : integer range 0 to N := 0;
+begin
+	process(clk, clear_contador)
+    begin
+		if (clear_contador = '1') then
+			-- clear_contador ligado, a saída vai para 0
+            count <= '0';
+            
+            -- a contagem deve ser reiniciada
+            contador <= 0;
+		elsif (rising_edge(clk)) then            
+            if (contador = N) then
+                -- indicar o fim do ciclo do contador na saída count
+            	count <= '1';
+                
+                -- como foi atingido o limite de contagem, contador é zerado
+                contador <= 0;
+            else
+            	-- o contador não é igual a N, deve ser incrementado
+				contador <= contador + 1;
+                
+                -- a saída count deve ser 0
+            	count <= '0';
+            end if;
+      end if;
+  end process;
+end architecture;
---
+++ blocos_logicos - Arthur Coronho, Lucas Szuster Murillo Kelvin/contador_de_0_ate_n - Arthur Coronho, Lucas Szuster Murillo Kelvin - Turma PN5/TESTBE~1.VHD
@@ -0,0 +1,50 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity tb_contador_n is
+end entity;
+
+architecture sim of tb_contador_n is
+	-- declarações de signals necessários
+	constant N : integer := 4;
+    signal clear_contador : std_logic := '0';
+    signal count : std_logic;
+    signal clk : std_logic := '1';
+    signal clk_enable : std_logic := '1';
+    constant tempo : time := 10 ns;
+begin
+	-- inicializando a uut e realizando o port map
+	uut: entity work.contador_n
+    generic map (
+    	N => N
+    )
+    
+    port map (
+        clear_contador => clear_contador,
+        clk => clk,
+        count => count
+    );
+    
+    -- inicializando o pulso do clock
+    clk <= clk_enable and not clk after tempo / 2;
+    
+    stim: process
+    begin
+		-- teste inicial
+        wait for tempo * 20;
+        
+        -- ligar o clear_contador
+        clear_contador <= '1';
+        wait for tempo * 20;
+        
+        -- desligar o clear_contador
+        clear_contador <= '0';
+        wait for tempo * 20;
+
+		--terminar o clock
+        clk_enable <= '0';
+		
+		-- fim da simulação
+        wait; 
+    end process;    
+end architecture;
