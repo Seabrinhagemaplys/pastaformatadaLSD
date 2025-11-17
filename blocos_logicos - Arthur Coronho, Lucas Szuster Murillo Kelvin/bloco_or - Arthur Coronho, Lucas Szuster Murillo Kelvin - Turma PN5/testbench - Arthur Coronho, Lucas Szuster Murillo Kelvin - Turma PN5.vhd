75afe38a89c581a32b2658d28cead72997384a0c
Seabrinhagemaplys <66424663+Seabrinhagemaplys@users.noreply.github.com>
2025-11-17 15:29:18 -0300

    Update testbench - Arthur Coronho, Lucas Szuster Murillo Kelvin - Turma PN5.vhd

--- blocos_logicos - Arthur Coronho, Lucas Szuster Murillo Kelvin/bloco_or - Arthur Coronho, Lucas Szuster Murillo Kelvin - Turma PN5/testbench - Arthur Coronho, Lucas Szuster Murillo Kelvin - Turma PN5.vhd
+++ blocos_logicos - Arthur Coronho, Lucas Szuster Murillo Kelvin/bloco_or - Arthur Coronho, Lucas Szuster Murillo Kelvin - Turma PN5/testbench - Arthur Coronho, Lucas Szuster Murillo Kelvin - Turma PN5.vhd
@@ -6,22 +6,23 @@ end entity;
 
 architecture arqsim of tb_bloco_or is
     --Declaração dos signals
-    signal entrada_um   :std_logic;
-    signal entrada_dois : std_logic;
-    signal saida        : std_logic;
-    constant tempo    : time := 10 ns;
-begin
-    	
-    -- Declarando a uut e realizando o port map
-    uut:entity work.bloco_or
-        
-    port map(
-        entrada_um => entrada_um,
-        entrada_dois => entrada_dois,
-        saida => saida
-    );
-    process
+    signal entrada_um       :std_logic;
+    signal entrada_dois     : std_logic;
+    signal saida            : std_logic;
+    constant tempo          : time := 10 ns;
+    signal clear_assincrono : std_logic := '0';
     begin
+    	
+        -- Declarando a uut e realizando o port map
+        uut:entity work.bloco_or
+        port map(
+        	entrada_um => entrada_um,
+            entrada_dois => entrada_dois,
+            saida => saida,
+            clear_assincrono => clear_assincrono
+        );
+        process
+        begin
         
         --Testes implementados
         entrada_um <= '0';
@@ -36,7 +37,21 @@ begin
         entrada_um <= '1';
         entrada_dois <= '1'; wait for tempo*5;
         
+        --Testes com clear assincrono ligado
+        clear_assincrono <='1';
+        entrada_um <= '0';
+        entrada_dois <= '0'; wait for tempo*5;
+        
+        entrada_um <= '1'; 
+        entrada_dois <= '0'; wait for tempo*5;
+        
+        entrada_um <= '0';
+        entrada_dois <= '1'; wait for tempo*5;
+        
+        entrada_um <= '1';
+        entrada_dois <= '1'; wait for tempo*5;
+        
         --Fim da simulação
         wait;
-    end process;
+        end process;
 end architecture;
