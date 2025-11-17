18184f19ae262be140cf792d2b4d49e4a3a47161
Seabrinhagemaplys <66424663+Seabrinhagemaplys@users.noreply.github.com>
2025-11-17 15:34:51 -0300

    Update design - Arthur Coronho, Lucas Szuster, Murillo Kelvin - Turma PN5.vhd

--- blocos_logicos - Arthur Coronho, Lucas Szuster Murillo Kelvin/calcular_virar_a_direita - Arthur Coronho, Lucas Szuster Murillo Kelvin - Turma PN5/design - Arthur Coronho, Lucas Szuster, Murillo Kelvin - Turma PN5.vhd
+++ blocos_logicos - Arthur Coronho, Lucas Szuster Murillo Kelvin/calcular_virar_a_direita - Arthur Coronho, Lucas Szuster Murillo Kelvin - Turma PN5/design - Arthur Coronho, Lucas Szuster, Murillo Kelvin - Turma PN5.vhd
@@ -5,9 +5,10 @@ use IEEE.std_logic_1164.all;
 entity calcular_virar_a_direita is
   port (
     -- entradas
-	pedestre : in std_logic;
-    outro_pedestre : in std_logic;
-    emergencia : in std_logic;
+	clear_assincrono : in std_logic;
+    pedestre         : in std_logic;
+    outro_pedestre   : in std_logic;
+    emergencia       : in std_logic;
     outra_emergencia : in std_logic;
     
     -- saídas
@@ -18,5 +19,6 @@ end entity;
 architecture calc of calcular_virar_a_direita is
 begin
 	-- pode virar a direita se nenhum pedestre está atravessando
-	virar_a_direita <= not(pedestre) and not(outro_pedestre) and not(emergencia) and not(outra_emergencia);	
+	virar_a_direita <= '0' when clear_assincrono = '1' else
+    not(pedestre) and not(outro_pedestre) and not(emergencia) and not(outra_emergencia);	
 end architecture;
